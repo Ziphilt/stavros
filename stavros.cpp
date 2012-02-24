91396cbe8c0ab99f336670f54a5d1ffcc9e3b650
Cal Stepanian <ziphilt@gmail.com>
2012-02-24 18:22:06 -0500

    added keyboard controls and more detailed dependency info

--- stavros.cpp
+++ stavros.cpp
@@ -23,27 +23,48 @@ int main()
   sf::Clock Clock;
 
   // set window coordinates
-  double x0 = -20.;
-  double x1 = 20.;
-  double y0 = -20.;
-  double y1 = 20.;
+  double x0 = -40.;
+  double x1 = 40.;
+  double y0 = -40.;
+  double y1 = 40.;
   double ratio = (y1-y0)/(x1-x0);
+
+  bool left  = false;
+  bool right = false;
+  bool up    = false;
+  bool down  = false;
+  bool a     = false;
+  bool e     = false;
+  cpVect thrust;
+  double force = 70;
+  cpVect leftThrust = cpv(-force,0);
+  cpVect rightThrust = cpv(force,0);
+  cpVect upThrust = cpv(0,force);
+  cpVect downThrust = cpv(0,-force); 
+  double angThrust;
+  double torque = 3;
+  double aTorque = torque;
+  double eTorque = -torque;
   
   cpSpace* space = cpSpaceNew();
-  cpSpaceSetGravity(space, cpv(0,-100));
-  cpShape *ground1 = cpSegmentShapeNew(space->staticBody, cpv(-20, 5), cpv(0, 0), 0);
-  cpShapeSetFriction(ground1, 1);
+  cpSpaceSetGravity(space, cpv(0,-10));
+  cpShape *ground1 = cpSegmentShapeNew(space->staticBody, cpv(-20, 5), cpv(-5, 5), 0);
+  cpShapeSetFriction(ground1, 10);
+  cpShapeSetElasticity(ground1, 0.2);
   cpSpaceAddShape(space, ground1);
-  cpShape *ground2 = cpSegmentShapeNew(space->staticBody, cpv(0, 0), cpv(20, 5), 0);
-  cpShapeSetFriction(ground2, 1);
+  cpShape *ground2 = cpSegmentShapeNew(space->staticBody, cpv(-5, 0), cpv(20, 0), 0);
+  cpShapeSetFriction(ground2, 0.0);
+  cpShapeSetElasticity(ground2, 0.5);
+  //cpShapeSetSurfaceVelocity(ground2, cpv(10,0));
   cpSpaceAddShape(space, ground2);
-  cpFloat radius = 5;
+  cpFloat radius = 1;
   cpFloat mass = 1;
   cpFloat moment = cpMomentForCircle(mass, 0, radius, cpvzero);
   cpBody *ballBody = cpSpaceAddBody(space, cpBodyNew(mass, moment));
-  cpBodySetPos(ballBody, cpv(7, 15));
+  cpBodySetPos(ballBody, cpv(0, 1));
   cpShape *ballShape = cpSpaceAddShape(space, cpCircleShapeNew(ballBody, radius, cpvzero));
-  cpShapeSetFriction(ballShape, 0.7);
+  cpShapeSetFriction(ballShape, 1.);
+  cpShapeSetElasticity(ballShape, 0.5);
 
   //{{{ OpenGL stuff
   glMatrixMode(GL_PROJECTION);
@@ -71,8 +92,10 @@ int main()
     while (App.GetEvent(Event)){
       if (Event.Type == sf::Event::Closed)
           App.Close();
+      
       if ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Escape))
           App.Close();
+      
       // keep aspect ratio
       if (Event.Type == sf::Event::Resized){
         double height = Event.Size.Height;
@@ -87,12 +110,18 @@ int main()
         else cerr << "something weird, windowRatio = " << windowRatio << ", width = " << width << ", height = " << height << endl; 
       }
     }
+    left  = App.GetInput().IsKeyDown(sf::Key::Left);
+    right = App.GetInput().IsKeyDown(sf::Key::Right);
+    up    = App.GetInput().IsKeyDown(sf::Key::Up);
+    down  = App.GetInput().IsKeyDown(sf::Key::Down);
+    a     = App.GetInput().IsKeyDown(sf::Key::A);
+    e     = App.GetInput().IsKeyDown(sf::Key::E);
     //}}}
 
     App.SetActive();
     glClear(GL_COLOR_BUFFER_BIT);
 
-    //{{{ draw axes
+    /*{{{ draw axes
     glBegin(GL_LINES);
     glColor3d(.7,.7,.7);
     glVertex2d(0.,-32.);
@@ -100,18 +129,26 @@ int main()
     glVertex2d(-32.,0.);
     glVertex2d(32.,0.);
     glEnd();
-    //}}}
-
+    }}}*/
+    cpVect gPos = cpBodyLocal2World(ballBody, cpv(1,0));
+    //cpVect appliedForce = cpBodyGetForce(ballBody);
+    cerr << gPos.x << " " << gPos.y << endl;
+    thrust = leftThrust*(cpFloat)left + rightThrust*(cpFloat)right + upThrust*(cpFloat)up + downThrust*(cpFloat)down;
+    angThrust = aTorque*(cpFloat)a + eTorque*(cpFloat)e;
+    cpBodyResetForces(ballBody);
+    cpBodySetForce(ballBody, thrust);
+    cpBodySetTorque(ballBody, angThrust);
     // cerr << 1/App.GetFrameTime() << endl;
     double dt = App.GetFrameTime();
     
     cpSpaceStep(space,dt);
     cpVect pos = cpBodyGetPos(ballBody);
     glColor3d(1,1,1);
-    glBegin(GL_LINE_STRIP);
+    glBegin(GL_LINES);
     glVertex2d(-20,5);
-    glVertex2d(0,0);
-    glVertex2d(20,5);
+    glVertex2d(-5,5);
+    glVertex2d(-5,0);
+    glVertex2d(20,0);
     glEnd();
     circle(pos, cpBodyGetAngle(ballBody), radius);
 
