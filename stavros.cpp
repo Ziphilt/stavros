bd16e588c5ac1ef1649511f43245c8ad0aaafd53
Cal Stepanian <ziphilt@gmail.com>
2012-03-01 20:19:34 -0500

    added stable navigation to destination in x and y coordinates, added ability to click to set destination, added openGLpos function

--- stavros.cpp
+++ stavros.cpp
@@ -11,6 +11,7 @@ using std::vector;
 
 //{{{ Declarations
 struct Thrusters {
+  Thrusters();
   Thrusters(double,double,double,double,double,double);
   double left;
   double right;
@@ -20,19 +21,27 @@ struct Thrusters {
   double rotR;
 };
 
-double lin(double, double, double);
+Thrusters AI(cpBody*);
 void circle(cpVect, double, double, Thrusters);
+cpVect openGLpos(int, int);
 const int circleSamples = 32;
 GLdouble circleVAR[circleSamples*2 + 4];
+double gravity = -10;
+double force = 20;
+cpVect dest = cpv(0,0);
 //}}}
 
 //{{{ Main
 int main()
 {
   //{{{ Initializations
-  sf::Window App(sf::VideoMode(680, 680, 32), "Stavros");
+  int windowX = 680;
+  int windowY = 680;
+  sf::Window App(sf::VideoMode(windowX, windowY, 32), "Stavros");
   App.SetFramerateLimit(100);
   sf::Clock Clock;
+  cerr.precision(3);
+  //cerr << std::ios_base::scientific;
 
   // set window coordinates
   double x0 = -10.;
@@ -48,7 +57,6 @@ int main()
   bool a     = false;
   bool e     = false;
   cpVect thrust;
-  double force = 10;
   cpVect leftThrust = cpv(-force,0);
   cpVect rightThrust = cpv(force,0);
   cpVect upThrust = cpv(0,force);
@@ -59,12 +67,12 @@ int main()
   double eTorque = -torque;
   
   cpSpace* space = cpSpaceNew();
-  //cpSpaceSetGravity(space, cpv(0,-10));
+  cpSpaceSetGravity(space, cpv(0, gravity));
   
-  cpShape *wall0 = cpSegmentShapeNew(space->staticBody, cpv(10, -10), cpv(10, 10), 0);  
-  cpShape *wall1 = cpSegmentShapeNew(space->staticBody, cpv(10, 10), cpv(-10, 10), 0);  
-  cpShape *wall2 = cpSegmentShapeNew(space->staticBody, cpv(-10, 10), cpv(-10, -10), 0);  
-  cpShape *wall3 = cpSegmentShapeNew(space->staticBody, cpv(-10, -10), cpv(10, -10), 0);
+  cpShape* wall0 = cpSegmentShapeNew(space->staticBody, cpv(10, -10), cpv(10, 10), 0);  
+  cpShape* wall1 = cpSegmentShapeNew(space->staticBody, cpv(10, 10), cpv(-10, 10), 0);  
+  cpShape* wall2 = cpSegmentShapeNew(space->staticBody, cpv(-10, 10), cpv(-10, -10), 0);  
+  cpShape* wall3 = cpSegmentShapeNew(space->staticBody, cpv(-10, -10), cpv(10, -10), 0);
   cpShapeSetFriction(wall0, 10);
   cpShapeSetFriction(wall1, 10);
   cpShapeSetFriction(wall2, 10);
@@ -79,11 +87,11 @@ int main()
   cpSpaceAddShape(space, wall3);
 
   cpFloat radius = 1;
-  cpFloat mass = 3;
+  cpFloat mass = 1;
   cpFloat moment = cpMomentForCircle(mass, 0, radius, cpvzero);
-  cpBody *ballBody = cpSpaceAddBody(space, cpBodyNew(mass, moment));
-  cpBodySetPos(ballBody, cpv(0, 1));
-  cpShape *ballShape = cpSpaceAddShape(space, cpCircleShapeNew(ballBody, radius, cpvzero));
+  cpBody* ballBody = cpSpaceAddBody(space, cpBodyNew(mass, moment));
+  cpBodySetPos(ballBody, cpv(0, -8));
+  cpShape* ballShape = cpSpaceAddShape(space, cpCircleShapeNew(ballBody, radius, cpvzero));
   cpShapeSetFriction(ballShape, 1.);
   cpShapeSetElasticity(ballShape, 0.5);
   
@@ -124,19 +132,19 @@ int main()
       
       if ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Escape))
           App.Close();
+
+      if (Event.Type == sf::Event::MouseButtonPressed)
+        dest = openGLpos(Event.MouseButton.X, Event.MouseButton.Y);
       
       // keep aspect ratio
       if (Event.Type == sf::Event::Resized){
         double height = Event.Size.Height;
         double width  = Event.Size.Width;
         double windowRatio = height / width;
-        if (windowRatio > ratio)
-          glViewport(0, 0, width, width * ratio);
-        else if (windowRatio < ratio)
-          glViewport(0, 0, height / ratio, height);
-        else if (windowRatio == ratio)
-          glViewport(0, 0, width, height);
-        else cerr << "something weird, windowRatio = " << windowRatio << ", width = " << width << ", height = " << height << endl; 
+        if (windowRatio > ratio)       {windowX = width;          windowY = width * ratio;}
+        else if (windowRatio < ratio)  {windowX = height / ratio; windowY = height;}
+        else if (windowRatio == ratio) {windowX = width;          windowY = height;}
+        glViewport(0, 0, windowX, windowY);
       }
     }
     left  = App.GetInput().IsKeyDown(sf::Key::Left);
@@ -146,7 +154,7 @@ int main()
     a     = App.GetInput().IsKeyDown(sf::Key::A);
     e     = App.GetInput().IsKeyDown(sf::Key::E);
     //}}}
-
+    
     App.SetActive();
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -160,9 +168,14 @@ int main()
     glEnd();
     }}}*/
     //cpVect appliedForce = cpBodyGetForce(ballBody);
+    /*
     thrust = leftThrust*(cpFloat)left + rightThrust*(cpFloat)right + upThrust*(cpFloat)up + downThrust*(cpFloat)down;
     Thrusters t((double)left,(double)right,(double)up,(double)down,(double)a,(double)e);
     angThrust = aTorque*(cpFloat)a + eTorque*(cpFloat)e;
+    */
+    Thrusters t = AI(ballBody);
+    thrust = leftThrust*t.left + rightThrust*t.right + upThrust*t.up + downThrust*t.down;
+    angThrust = aTorque*t.rotL + eTorque*t.rotR;
     cpBodyResetForces(ballBody);
     cpBodyApplyForce(ballBody, cpvrotate(thrust, ballBody->rot), cpvzero);
     cpBodySetTorque(ballBody, angThrust);
@@ -171,6 +184,19 @@ int main()
     double dt = App.GetFrameTime();
     
     cpSpaceStep(space,dt);
+    // draw destination reticle
+    glColor3d(0,1,0);
+    glBegin(GL_LINES);
+    glVertex2d(dest.x - 3,dest.y);
+    glVertex2d(dest.x - 2,dest.y);
+    glVertex2d(dest.x + 2,dest.y);
+    glVertex2d(dest.x + 3,dest.y);
+
+    glVertex2d(dest.x,dest.y - 3);
+    glVertex2d(dest.x,dest.y - 2); 
+    glVertex2d(dest.x,dest.y + 2);
+    glVertex2d(dest.x,dest.y + 3);
+    glEnd();
     circle(cpBodyGetPos(ballBody), cpBodyGetAngle(ballBody), radius, t);
 
     App.Display();
@@ -189,10 +215,56 @@ int main()
 //}}}
 
 //{{{ Functions
-double lin(double t0, double t1, double t){
-  return t0 * t + t1 * (1-t);
+Thrusters AI(cpBody* b){
+  double m = cpBodyGetMass(b);
+  double maxDownA = -force/m + gravity; // falling
+  double maxUpA = force/m + gravity; // ascending
+  double maxLatA = force/m;
+  if (maxUpA < 0) cerr << "Thrust not powerful enough!" << endl;
+  // thruster setting appropriate for opposing gravity
+  double gravT = -gravity*m/force;
+  cpVect pos = cpBodyGetPos(b);
+  //double ang = cpBodyGetAngle(b);
+  cpVect vel = cpBodyGetVel(b);
+  //double angVel = cpBodyGetAngVel(b);
+  Thrusters t(0,0,0,0,0,0);
+  cpVect d = cpv(dest.x - pos.x, dest.y - pos.y);
+  // acceleration required to stop at dest
+  cpVect requiredA = cpv(-vel.x*vel.x/(2*d.x), -vel.y*vel.y/(2*d.y));
+  double err = 0.1;
+  double k = 4;
+  double damp = 0.4;
+
+  // control x position
+  if (abs(d.x) < err && abs(vel.x) < err){
+    double lat = cpfclamp(k*d.x - damp*vel.x,-1,1); // hold position
+    (lat > 0) ? t.right = lat : t.left = -lat;
+  }
+  else if (d.x > 0){
+    if (vel.x > 0 && abs(requiredA.x) > maxLatA)t.left = 1;
+    else t.right = 1;
+  }
+  else if (d.x < 0){
+    if (vel.x < 0 && abs(requiredA.x) > maxLatA)t.right = 1;
+    else t.left = 1;
+  }
+
+  // control y position
+  if (abs(d.y) < err && abs(vel.y) < err){
+    t.up = cpfclamp(gravT + k*d.y - damp*vel.y,0,1); // hover
+  }
+  else if (d.y > 0){
+    if (vel.y > 0 && requiredA.y < maxDownA) t.down = 1;
+    else t.up = 1;
+  }
+  else if (d.y < 0){
+    if (vel.y < 0 && requiredA.y > maxUpA) t.up = 1;
+    else t.down = 1;
+  }
+  return t;
 }
 
+//{{{ draw a circle
 void circle(cpVect pos, double angle, double radius, Thrusters t){
   glVertexPointer(2, GL_DOUBLE, 0, circleVAR);
   glPushMatrix();
@@ -231,7 +303,28 @@ void circle(cpVect pos, double angle, double radius, Thrusters t){
       glVertex2d(-t.rotL,-1);
     glEnd();
   glPopMatrix();
+}//}}}
+
+cpVect openGLpos(int x, int y){
+  GLint viewport[4];
+  GLdouble modelview[16];
+  GLdouble projection[16];
+  GLfloat winX, winY;
+  GLdouble posX, posY, posZ;
+
+  glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+  glGetDoublev(GL_PROJECTION_MATRIX, projection);
+  glGetIntegerv(GL_VIEWPORT, viewport);
+
+  winX = (float)x;
+  winY = (float)viewport[3] - (float)y;
+  //glReadPixels( x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ );
+
+  gluUnProject( winX, winY, 0, modelview, projection, viewport, &posX, &posY, &posZ);
+
+  return cpv(posX, posY);
 }
 
+Thrusters::Thrusters(){}
 Thrusters::Thrusters(double le,double ri,double u,double d,double rL,double rR): left(le),right(ri),up(u),down(d),rotL(rL),rotR(rR) {}
 //}}}
